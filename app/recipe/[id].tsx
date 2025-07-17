6e28d50fc12e69a7bb8823238403a66e4178c527
MarchewkaMatthew <56259093+MarchewkaMatthew@users.noreply.github.com>
2025-07-17 12:11:45 +0200

    Updated [id].tsx

--- app/recipe/[id].tsx
+++ app/recipe/[id].tsx
@@ -309,24 +309,15 @@ export default function RecipeDetailScreen() {
       {/* {showSuccessOverlay && ( */}
         <Modal
           visible={true}
-          transparent={true}
           animationType="fade"
           onRequestClose={handleCloseOverlay}
         >
-          <Animated.View style={[styles.successOverlay, { opacity: fadeAnim }]}>
-            <TouchableOpacity 
-              style={styles.overlayBackground} 
-              onPress={handleCloseOverlay}
-              activeOpacity={1}
-            >
               <View style={styles.successCard}>
                 <Text style={styles.successTitle}>Here is your new recipe</Text>
                 <Text style={styles.successDescription}>
                   Great choice! You've reduced the carbon footprint of this recipe by 60%
                 </Text>
               </View>
-            </TouchableOpacity>
-          </Animated.View>
         </Modal>
       {/* )} */}
       
