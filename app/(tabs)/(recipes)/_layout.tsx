f96543295a54caf4e5d69a2e8765bbc2bf5494eb
MarchewkaMatthew <56259093+MarchewkaMatthew@users.noreply.github.com>
2025-07-17 11:29:13 +0200

    Fix Recipe Navigation Structure

---
+++ app/(tabs)/(recipes)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+
+export default function RecipesLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="[id]" />
+    </Stack>
+  );
+}
